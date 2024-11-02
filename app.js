4ee54075f32c4e8f8685eadb03c0dce3a95c4e49
Alfatih <rahmatalfatih04@gmail.com>
2024-11-02 12:18:15 +0700

    Revert "fitur login dan admin namun belum semua"
    This reverts commit f2d78694246d39ccee001faad549426e1d774f40.

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- database/database.js
+++ database/database.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 let mysql = require('mysql');
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'db_manajemensampah',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATA
 })
 connection.connect((error)=>{
     if(!!error){
