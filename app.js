1173c6dafd855d369740e915f419650ce4b1f864
GodSpeeds27 <tuwowoxnl@gmail.com>
2024-11-04 19:35:32 +0700

    add more by danang

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var flash = require('express-flash')
+var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
+
 
 var app = express();
 
@@ -23,6 +26,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  cookie: {
+    maxAge: 10 * 60 * 1000
+  },
+  store: new session.MemoryStore,
+  saveUninitialized: true,
+  resave: 'true',
+  secret: 'secret'
+}))
+app.use(flash())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
